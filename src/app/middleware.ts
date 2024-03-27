284325e3b14e725f25ba66d4538cc0b42811c8b8
unknown <adhabal2002@gmail.com>
2024-03-27 19:50:30 +0530

    fix

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -4,7 +4,7 @@ export function middleware() {
     const res = NextResponse.next()
 
     res.headers.append('Access-Control-Allow-Credentials', "true")
-    res.headers.append('Access-Control-Allow-Origin', '*') 
+    res.headers.append('Access-Control-Allow-Origin', 'https://bhabhaai.vercel.app/') 
     res.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
     res.headers.append(
         'Access-Control-Allow-Headers',
